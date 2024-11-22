5dfe92e4fe6c8dcc1ff3612fc08ef9b91c6a3542
daydreaam <dayingyeo@naver.com>
2024-11-22 16:08:57 +0900

    테스트코드 작성

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,12 +5,8 @@ class App {
         Repository repository = new Repository();
         System.out.println("== 명언 앱 ==");
         int lastId = repository.lastNumber();
-        // lastId 받아오는 과정 필요 (Repository)
-
         Controller controller = new Controller();
         lastId = controller.run(lastId);
-
-        // lastId 저장 (Repository)
         repository.saveFile("lastId.txt",String.valueOf(lastId));
     }
 }
--- src/test/java/com/ll/TestUtil.java
+++ src/test/java/com/ll/TestUtil.java
@@ -1,3 +1,5 @@
+package com.ll;
+
 import java.io.*;
 import java.util.Scanner;
 
